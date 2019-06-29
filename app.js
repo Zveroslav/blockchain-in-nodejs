0358fac6bb7dc95e5cd736ac482ee87371575f62
Zveroslav <zveroslav@yahoo.com>
2019-06-29 18:33:46 +0300

    Added chainCalculator

--- app.js
+++ app.js
@@ -15,3 +15,44 @@ class Block {
 }
 
 
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()]
+  }
+
+  createGenesisBlock(){
+    return new Block(0, '01/07/2019', 'Genesis Block', '0')
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  addBlock(newBlock) {
+    newBlock.perviousHash = this.getLastBlock();
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock)
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[this.chain.length - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+
+      if (previousBlock.hash !== previousBlock.hash){
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let savejeeCoin = new BlockChain();
+savejeeCoin.addBlock(new Block(1, Date.now(), {amount: 4}));
+savejeeCoin.addBlock(new Block(2, Date.now(), {amount: 5}));
+
